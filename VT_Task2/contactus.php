a801b3ec744fbcb96d0f4089ced6d8b5ab28448d
Asterxxxx <viktor.trilar@student.hamk.fi>
2024-01-29 11:03:08 +0200

    Universal header & footer, task pages 2-7 created

---
+++ VT_Task2/contactus.php
@@ -0,0 +1,12 @@
+<?php 
+$title = "Contact Us";
+
+
+include 'header.php'; ?>
+
+<h2>Contact Us</h2>
+
+
+
+
+<?php include 'footer.php'; ?>
---
+++ VT_Task2/page2.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Another title";
+include 'header.php';  ?>
+<h2>New page content</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam officiis magni veniam fugiat debitis atque fugit placeat, aperiam tempore laboriosam.
+     Veritatis, minus corporis qui officiis pariatur beatae recusandae velit nesciunt.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ad porro eius voluptas voluptatem quae assumenda pariatur sequi, 
+    consequatur modi vero sapiente eveniet itaque voluptates, cumque optio quibusdam, maxime eaque.
+</p>
+
+
+<?php include 'footer.php'; ?>
