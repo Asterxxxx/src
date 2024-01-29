069930d9773ac5405727d854329f26c8fb894dfc
Asterxxxx <viktor.trilar@student.hamk.fi>
2024-01-29 11:18:41 +0200

    Moving files

---
+++ VT_TasksWebsite/Task4.php
@@ -0,0 +1,33 @@
+<?php 
+$title = "Task 4 - Viktor Trilar";
+include 'header.php'; ?>
+
+<body>
+    <!-- Home body container -->
+<div class="body-container">
+
+<!-- Banner -->
+    <div class="home-banner-container">
+        <img src="images/phpbanner.jpg" alt="php-banner" draggable="false">
+    </div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum repellendus iste, officiis repellat numquam fugit distinctio ad veritatis pariatur nostrum error qui sit nulla impedit culpa iure omnis facere quos.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate voluptate exercitationem, illum pariatur laboriosam aliquid amet quaerat ipsam perferendis totam eius delectus quod dolorum suscipit iste nam in mollitia.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quo vero nemo minus nihil totam ex. Ducimus facere odit, hic, dignissimos ab magnam illum quaerat quam laudantium quasi cum sunt?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aspernatur magnam porro deserunt nam tempora, suscipit pariatur vero cupiditate, tempore velit obcaecati illum aliquam quas minus quae vel. Fugiat, aperiam?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est nisi ipsum neque placeat, id vel ut. Unde illum ad blanditiis quia possimus hic ipsam consectetur maxime, nesciunt praesentium odio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum repellendus iste, officiis repellat numquam fugit distinctio ad veritatis pariatur nostrum error qui sit nulla impedit culpa iure omnis facere quos.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate voluptate exercitationem, illum pariatur laboriosam aliquid amet quaerat ipsam perferendis totam eius delectus quod dolorum suscipit iste nam in mollitia.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quo vero nemo minus nihil totam ex. Ducimus facere odit, hic, dignissimos ab magnam illum quaerat quam laudantium quasi cum sunt?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aspernatur magnam porro deserunt nam tempora, suscipit pariatur vero cupiditate, tempore velit obcaecati illum aliquam quas minus quae vel. Fugiat, aperiam?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est nisi ipsum neque placeat, id vel ut. Unde illum ad blanditiis quia possimus hic ipsam consectetur maxime, nesciunt praesentium odio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum repellendus iste, officiis repellat numquam fugit distinctio ad veritatis pariatur nostrum error qui sit nulla impedit culpa iure omnis facere quos.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate voluptate exercitationem, illum pariatur laboriosam aliquid amet quaerat ipsam perferendis totam eius delectus quod dolorum suscipit iste nam in mollitia.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quo vero nemo minus nihil totam ex. Ducimus facere odit, hic, dignissimos ab magnam illum quaerat quam laudantium quasi cum sunt?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aspernatur magnam porro deserunt nam tempora, suscipit pariatur vero cupiditate, tempore velit obcaecati illum aliquam quas minus quae vel. Fugiat, aperiam?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est nisi ipsum neque placeat, id vel ut. Unde illum ad blanditiis quia possimus hic ipsam consectetur maxime, nesciunt praesentium odio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum repellendus iste, officiis repellat numquam fugit distinctio ad veritatis pariatur nostrum error qui sit nulla impedit culpa iure omnis facere quos.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores cupiditate voluptate exercitationem, illum pariatur laboriosam aliquid amet quaerat ipsam perferendis totam eius delectus quod dolorum suscipit iste nam in mollitia.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quo vero nemo minus nihil totam ex. Ducimus facere odit, hic, dignissimos ab magnam illum quaerat quam laudantium quasi cum sunt?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aspernatur magnam porro deserunt nam tempora, suscipit pariatur vero cupiditate, tempore velit obcaecati illum aliquam quas minus quae vel. Fugiat, aperiam?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est nisi ipsum neque placeat, id vel ut. Unde illum ad blanditiis quia possimus hic ipsam consectetur maxime, nesciunt praesentium odio.
+</body>
+
+<?php include 'footer.php'; ?>
---
+++ VT_TasksWebsite/footer.php
@@ -0,0 +1,52 @@
+<!-- Footer -->
+<footer class="footer-container">
+        <section class="col-1">
+            <p>QUICK LINKS</p>
+            <nav>
+                <ul>
+                    <br><li><a href="Task1.php">Task 1</a></li>
+                    <li><a href="Task2.php">Task 2</a></li>
+                    <li><a href="Task3.php">Task 3</a></li>
+                    <li><a href="Task4.php">Task 4</a></li>
+                    <li><a href="Task5.php">Task 5</a></li>
+                    <li><a href="Task6.php">Task 6</a></li>
+                    <li><a href="Task7.php">Task 7</a></li>
+                    
+                </ul>
+            </nav>
+        </section>
+        <section class="col-2">
+            <p>ADOPT OR GET INVOLVED</p>
+            <nav>
+                <ul>
+                    <br><li><a href="#">Adoption Process</a></li>
+                    <li><a href="#">Available Cats</a></li>
+                    <li><a href="#">Foster a Cat</a></li>
+                    <li><a href="#">Volunteer Opportunities</a></li>
+                    <li><a href="#">Donate</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Sponsor a Cat</a></li>
+                    <li><a href="#">Partner with Us</a></li>
+                </ul>
+            </nav>
+        </section>
+        <section class="col-3">
+            <p>FOLLOW US</p>
+            <nav>
+                <ul>
+                    <br><li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Pinterest</a></li>
+                    <li><a href="#">Newsletter</a></li>
+                </ul>  
+            </nav>
+        </section>
+        <section class="col-4">
+            <div class="image">
+                <a href="home.html"><img src="images/logo-2.png"></a>
+            </div>
+        </section>
+    </footer>
+
+</html>
